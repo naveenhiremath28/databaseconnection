6178c2c413fe12b9a7d1dbdef48bb863ad77df85
naveenhiremath28 <naveenhiremath28@gmail.com>
2025-01-13 17:42:32 +0530

    added middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,29 @@
+from fastapi.responses import JSONResponse
 import uvicorn
-from fastapi import FastAPI
-from routes.db_routes import router
+from fastapi import FastAPI, Request, HTTPException
+from app.routes.db_routes import db_router
+import time
+import asyncio
+from jwt.exceptions import InvalidTokenError
+from app.jwt_handler.auth import get_current_active_user
+from app.exceptions.app_exceptions import CredentialsException
 
 app = FastAPI()
 
-app.include_router(router)
+app.include_router(db_router)
+
+@app.middleware("http")
+async def process_time(request: Request, call_next):
+    if request.url.path.startswith("/connection/"):
+        try:
+            if get_current_active_user():
+                response = await call_next(request)
+                return response
+        except CredentialsException as e:
+            return JSONResponse({"detail": e.detail}, status_code=e.status_code)
+    else:
+        response = await call_next(request)
+        return response
 
 
 if __name__ == "__main__":
